94edd4a9cac27898c2d8358afccf71f77b086471
xing8star <xing0star@live.hk>
2024-08-22 22:55:29 +0800

    0.1.1: performance optimize

--- src/util.jl
+++ src/util.jl
@@ -1,4 +1,5 @@
-splitat(x::Vector,idx)=x[1:idx],x[idx+1:end]
-function swap_offset_plusone!(s::Vector,i,j)
+splitat(x::AbstractVector,idx)=x[1:idx],x[idx+1:end]
+splitat_view(x::AbstractVector,idx)=@views x[1:idx],x[idx+1:end]
+function swap_offset_plusone!(s::AbstractVector,i,j)
     s[[i+1,j+1]]=s[[j+1,i+1]]
 end
